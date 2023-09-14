d279d8e4d5fe5b909af2f72a64713cbd6898322b
jiangsongyang <2744856707@qq.com>
2023-09-14 11:16:09 +0800

    print

---
+++ 01-print/index.py
@@ -0,0 +1,18 @@
+""" 打印到文件 """
+
+# fp = open('./01-print/text.txt' , 'a+')
+
+# print('hello world' , file=fp)
+
+# fp.close()
+
+""" 转义字符 """
+
+print('hello\nworld')
+print('hello\tworld')
+print('hello\rworld')
+print('hello\bworld')
+print('hello\'world')
+
+# 元字符
+print(r'hello\nworld')
