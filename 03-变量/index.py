1ee48afeeeb50b2b40f8b44e0f401a7ba62f2459
jiangsongyang <2744856707@qq.com>
2023-09-14 11:42:17 +0800

    varable

---
+++ 03-变量/index.py
@@ -0,0 +1,39 @@
+# str = 'jsy'
+
+# print(str , id(str) , type(str))
+
+# str = 'jsy'
+# print(str , id(str) , type(str))
+
+# str = 'jsy cool'
+# print(str , id(str) , type(str))
+
+# str = 123
+# print(str , id(str) , type(str))
+
+""" 基础类型 """
+# num = 0b001
+# print(num , id(num) , type(num))
+# flt = 1.1
+# print(flt , id(flt) , type(flt))
+# bool = True
+# print(bool , id(bool) , type(bool))
+# str = 'jsy'
+# print(str , id(str) , type(str))
+
+""" 精度不准 """
+# import decimal
+
+# print(1.1 + 2.2)
+# print(decimal.Decimal('1.1') + decimal.Decimal('2.2'))
+
+""" 类型转换 """
+
+print(str(1))
+print(str(1.1))
+
+print(int('1'))
+print(int(1.1))
+
+print(float('1.1'))
+print(float(1))
